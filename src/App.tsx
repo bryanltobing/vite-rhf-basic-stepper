524433d6058f1c7cd5f3cf812f7e7bb51a31b74f
bryantobing12 <bryantobing0@gmail.com>
2022-08-26 18:37:37 +0700

    feat: add zod and hookform resolver integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,46 @@
-import { useState } from 'react'
-import { FieldErrors, SubmitHandler, useForm } from 'react-hook-form'
+import { useMemo, useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-type FormValues = {
-  username: string
-  password: string
-  name: string
-  email: string
-  website: string
-  github: string
-}
+const firstStepSchema = z.object({
+  username: z.string().min(1, { message: 'Username is required' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must include at least 6 characters' }),
+})
+const secondStepSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required' }),
+  email: z.string().regex(/^\S+@\S+$/, { message: 'Email is invalid' }),
+})
+const lastStepSchema = z.object({
+  website: z.string().min(1, { message: 'Website is required' }),
+  github: z.string().min(1, { message: 'Github is required' }),
+})
+
+const schema = firstStepSchema.and(secondStepSchema).and(lastStepSchema)
+
+type FormValues = z.infer<typeof schema>
 
 function App() {
   const [activeStep, setActiveStep] = useState(0)
   const lastStepIndex = 2
+
+  const usedSchema = useMemo(() => {
+    if (activeStep === 0) {
+      return firstStepSchema
+    }
+
+    if (activeStep === 1) {
+      return secondStepSchema
+    }
+
+    // Last step
+    return schema
+  }, [activeStep])
 
   const form = useForm<FormValues>({
     defaultValues: {
@@ -26,73 +51,7 @@ function App() {
       website: '',
       github: '',
     },
-    resolver: async (data) => {
-      let values: FormValues | {} = {}
-      let errors: FieldErrors<FormValues> = {}
-
-      if (activeStep === 0) {
-        if (!data.username) {
-          values = {}
-          errors.username = {
-            type: 'required',
-            message: 'Username is required',
-          }
-        } else {
-          values = data
-          errors = {}
-        }
-        if (data.password.length < 6) {
-          values = {}
-          errors.password = {
-            type: 'minLength',
-            message: 'Password must include at least 6 characters',
-          }
-        } else {
-        }
-      }
-
-      if (activeStep === 1) {
-        if (!data.name) {
-          values = {}
-          errors.name = {
-            type: 'required',
-            message: 'Name is required',
-          }
-        }
-
-        if (!/^\S+@\S+$/.test(data.email)) {
-          values = {}
-          errors.email = {
-            type: 'pattern',
-            message: 'Email is invalid',
-          }
-        }
-      }
-
-      if (activeStep === 2) {
-        if (!data.website) {
-          values = {}
-          errors.website = {
-            type: 'required',
-            message: 'Website is required',
-          }
-        }
-        if (!data.github) {
-          values = {}
-          errors.github = {
-            type: 'required',
-            message: 'Github is required',
-          }
-        }
-
-        // PUT THIS IN LAST STEP
-        if (Object.keys(errors).length < 1) {
-          values = data
-        }
-      }
-
-      return { values, errors }
-    },
+    resolver: zodResolver(usedSchema),
   })
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
@@ -161,8 +120,18 @@ function App() {
             ))}
           </ul>
 
+          {activeStep !== 0 && (
+            <button
+              className="btn-secondary"
+              type="button"
+              onClick={() => setActiveStep((prevStep) => prevStep - 1)}
+            >
+              &lt; Back
+            </button>
+          )}
+
           <button type="submit">
-            {activeStep === lastStepIndex ? 'Submit' : 'Next'}
+            {activeStep === lastStepIndex ? 'Submit' : 'Next >'}
           </button>
         </div>
       </form>
